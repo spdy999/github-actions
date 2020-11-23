1c20c3702c78eebbbc81157bade8f17f8a83a43d
spdy999 <1a1a1a.262626.0010@gmail.com>
2020-11-24 03:00:22 +0700

    test get data from api, configure cors in api

--- TrickingLibrary.Api/Startup.cs
+++ TrickingLibrary.Api/Startup.cs
@@ -1,15 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace TrickingLibrary.Api
 {
@@ -21,20 +14,22 @@ namespace TrickingLibrary.Api
         }
 
         public IConfiguration Configuration { get; }
+        private const string AllCors = "All";
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+                options.AddPolicy(AllCors, build => build.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+
+            app.UseCors(AllCors);
 
             app.UseHttpsRedirection();
 
